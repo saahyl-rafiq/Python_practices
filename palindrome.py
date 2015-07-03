f82aa3eb5e5f9bad01d1ae54e21f54bba6b1ff2b
Saahyl Rafiq <saahylrafiq@gmail.com>
2015-07-03 11:43:31 +0530

    Palindromes in string

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,12 @@
 def palin():
-	str=raw_input('Enter a string:')
-	lst=str.split(' ')
+	line=raw_input('Enter a string:')
+	lst=line.split(' ')
 	for i in lst:
 		if i==i[::-1]:
 			print "Palindrome: %r == %r " % (i, i[::-1])
 			#print "palindrome:",i "==", i
 		else:
-			print "			   %r != %r " % (i, i[::-1])
+			print "Not Palindrome: %r != %r " % (i, i[::-1])
 			#print "           ", i "!=", i[::-1]
 
 palin()
